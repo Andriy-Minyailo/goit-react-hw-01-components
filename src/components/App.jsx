4623e91098d849953f7797313ddedd6f862a1918
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-05-20 19:05:12 +0300

    task4-completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
 import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
 
 // console.log(user);
 export const App = () => {
@@ -19,7 +22,6 @@ export const App = () => {
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
-        // backgroundColor: '#787979',
       }}
     >
       <Profile
@@ -31,6 +33,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
